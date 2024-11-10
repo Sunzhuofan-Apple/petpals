fa01ae2ae0a44c1f57ed87a51fc6bd241d4aec33
miaoyyeah <miaoyany@andrew.cmu.edu>
2024-11-10 16:10:44 -0500

    add windows autosetup

---
+++ start.bat
@@ -0,0 +1,13 @@
+@echo off
+
+REM Activate virtual environment
+call venv\Scripts\activate
+
+REM Install dependencies
+pip install -r requirements.txt
+
+REM Run database migrations
+python petpal\manage.py migrate
+
+REM Start Django development server
+python petpal\manage.py runserver
