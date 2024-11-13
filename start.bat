a1a1bd2b56e82d0f807e742ac9a57c701068c1a7
miaoyyeah <miaoyany@andrew.cmu.edu>
2024-11-13 11:34:38 -0500

    update auto start in windows

--- start.bat
+++ start.bat
@@ -7,6 +7,7 @@ REM Install dependencies
 pip install -r requirements.txt
 
 REM Run database migrations
+python petpal\manage.py makemigrations
 python petpal\manage.py migrate
 
 REM Start Django development server
